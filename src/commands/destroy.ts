6e02655caac9c8e66bad2facbc098ac04f8f7ffd
Danil1979 <1979Cloud@gmail.com>
2019-10-09 23:58:25 +0800

    destroy function

---
+++ src/commands/destroy.ts
@@ -0,0 +1,35 @@
+import * as Discord from "discord.js";
+import {IBotCommand} from "../api";
+// require('dotenv').config();
+export default class destroy implements IBotCommand {
+
+    private readonly _command = "destroy";
+
+    help(): string {
+        return "destroy";
+    }  
+    
+    isThisCommand(command: string): boolean {
+       return command === this._command;
+    }
+
+    async runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): Promise<void> {
+        //if bot is in a channel
+
+        if(msgObject.guild.voiceConnection){
+            msgObject.channel.send(">>> I can't carry out that function right now because I'm in a voice channel!");
+            return;
+        }
+        //If user is not me(Danil)
+        if(msgObject.member.id!='222728476816310272'){
+            msgObject.channel.send("Only Danil can use this command!");
+            return;
+        }
+        msgObject.channel.send(">>> Restarting...");
+        client.destroy();
+        return;
+    }
+
+
+
+}
