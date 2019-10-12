c4c861bca1590950a944d468ac44fba158be8471
Danil1979 <1979Cloud@gmail.com>
2019-10-13 03:07:05 +0800

    prefix fixed

--- dist/config.js
+++ dist/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
     "token": "",
-    "prefix": ";",
+    "prefix": "~",
     "commands": [
         "kick",
         "prune",
