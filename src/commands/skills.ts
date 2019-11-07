4d208ae850c1f636ca76aa19807153525f1eb922
Wyyee Tan <1979Cloud@Gmail.com>
2019-11-08 00:31:36 +0800

    skills

--- src/commands/skills.ts
+++ src/commands/skills.ts
@@ -1,13 +1,13 @@
 import * as Discord from "discord.js";
 import {IBotCommand} from "../api";
-import update, { initialize } from "./update";
+import update from "./update";
 const queue = new Map();
 export default class skills implements IBotCommand {
 
     private readonly _command = "skills";
 
     help(): string {
-        return "~skills shikiname|to search for a shiki's skill set.\n";
+        return "skills";
     }  
     
     isThisCommand(command: string): boolean {
@@ -15,89 +15,96 @@ export default class skills implements IBotCommand {
     }
 
     async runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): Promise<void> {
-
-      const shikiQuery = args.join(" ");
-      if(!args[0]){
-        return;
-    }
-
-
-    this.accessSheet(shikiQuery,msgObject,false);
-    }
-    async accessSheet(shikiQuery:string,msgObject:Discord.Message,retried:boolean){
-     const serverQueue = queue.get(msgObject.guild.id);
-     const shikiQueue = new Map();
-      var shiki:boolean=false;
-      var queryArray=[];
-
-      for(var i=0;i<update.skillArray.length;i++){
-              const aliasArray:string[] =update.skillArray[i][1].split(",");
-              for(let x=0;x<aliasArray.length;x++){
-                if(aliasArray[x].charAt(0)==" "){
-                  aliasArray[x]=aliasArray[x].substring(1);
-              }
-                  aliasArray[x]=aliasArray[x].toLowerCase();
-              }
-              if(update.skillArray[i][0].toLowerCase()==shikiQuery.toLowerCase()){
-                   queryArray.push(update.skillArray[i]);
-                   shikiQueue.set(update.skillArray[i][0],true);
-                   shiki=true;
-                  break;
-              }
-            
-              if(aliasArray.indexOf(shikiQuery.toLowerCase())!=-1){
-                queryArray.push(update.skillArray[i]);
-                shikiQueue.set(update.skillArray[i][0],true);
-                shiki=true;
-                  break;
-           }     
+        const shikiQuery = args.join(" ");
+        if(!args[0]){
+          return;
       }
-     const shikiCheck= shikiQueue.get(update.skillArray[i][0]);
+      accessSheet(shikiQuery,msgObject,false);
+    }
 
 
-     //possible redundant
-      for(let x=i+1;x<update.skillArray.length;x++){
+
+} 
+
+function accessSheet(shikiQuery:string,msgObject:Discord.Message,retried:boolean){
+    let shikiArray=[];
+    let shiki:boolean=false;
+    for(var i=0;i<update.skillArray.length;i++){
+        const aliasArray:string[] =update.skillArray[i][1].split(",");
+        
+        for(let x=0;x<aliasArray.length;x++){
+            aliasArray[x]=aliasArray[x].trim();
+            aliasArray[x]=aliasArray[x].toLowerCase();
+        }
+
+        
+        if(update.skillArray[i][0].toLowerCase()==shikiQuery.toLowerCase()){
+            shikiArray.push(update.skillArray[i]);
+            //  shikiQueue.set(update.skillArray[i][0],true);
+             shiki=true;
+            break;
+        }
+      
+        if(aliasArray.indexOf(shikiQuery.toLowerCase())!=-1){
+            shikiArray.push(update.skillArray[i]);
+        //   shikiQueue.set(update.skillArray[i][0],true);
+          shiki=true;
+            break;
+     }     
+}
+if(shiki){
+    console.log("shiki found");
+
+    for(let x=i+1;x<update.skillArray.length;x++){
         if(update.skillArray[x][0]!=""){
-
+    
           break;
         }
-        queryArray.push(update.skillArray[x]);
+        shikiArray.push(update.skillArray[x]);
       }
-   
-      if(shiki&&shikiCheck){
-        console.log("shikiCheck pass");
-        var tempArray=[];
-        var skill=queryArray[0][3];
-          for(let i=0;i<queryArray.length;i++){
-              if(queryArray[i][3]==skill||queryArray[i][3]==""){
+
+      let tempArray = [];
+      let skillArray =[];
+      let skillName= shikiArray[0][3];
+      for(let i=0;i<shikiArray.length;i++){
+
+        if(shikiArray[i][3]==skillName||shikiArray[i][3]==""){
  
-                tempArray.push(queryArray[i]);
+            tempArray.push(shikiArray[i]);
 
-              }else{
-                //when its not the same skill
-                
-                skill=queryArray[i][3];
-                //empty tempArray
-                tempArray.splice(0,queryArray.length);
+          }else{
+            //when its not the same skill
+            skillArray.push(tempArray.slice(0));
 
-                tempArray.push(queryArray[i]);
-    
-              }
-           
+            skillName=shikiArray[i][3];
+            //empty tempArray
+            tempArray.splice(0,shikiArray.length);
+
+            tempArray.push(shikiArray[i]);
+
           }
-          this.format(tempArray,msgObject);
-      }else if(!shiki&&retried){
-        msgObject.channel.send(`Sorry, I can't find the Shikigami named ${shikiQuery} to display it's skills.`)
-        return;
-    }else if(!shiki&&!retried){
-        await initialize();
-        this.accessSheet(shikiQuery,msgObject,true);  
-        };
- 
-  }
-//|Shiki name	|Alias|	number of skills|	skill|	SKILL upgrade|	skill level|	type|	coldown	|onibi|thumbnail|skillname|note name|note description
 
-  format(tempArray:any[][],msgObject:Discord.Message){
+
+      }
+      skillArray.push(tempArray.slice(0));
+      queue.set(msgObject.guild.id,skillArray);
+      const embedArray:Discord.RichEmbed[]=[];
+      skillArray.forEach(embed => {
+         embedArray.push(format(embed));
+      });
+
+ 
+      sendMessage(embedArray,msgObject);
+
+
+
+}else{
+    msgObject.channel.send(`Sorry, I can't find the Shikigami named ${shikiQuery} to display it's skills.`)
+}
+
+
+}
+function format(tempArray:any[][]):Discord.RichEmbed{
     const shikiSkill={
       name:tempArray[0][0],
       skillDescription:tempArray[0][3],
@@ -117,21 +124,27 @@ export default class skills implements IBotCommand {
       }
     }
 
-    var skillLevel="";
-    var skillUpgrade="";
+    // var skillLevel="";
+    // var skillUpgrade="";
+    var skillDisplay="";
     var level="Level";
     var effect="Effect";
     if(tempArray[0][5]==""){ 
-      skillLevel="\u200b";
-      skillUpgrade="\u200b";
+    //   skillLevel="\u200b";
+    //   skillUpgrade="\u200b";
+      skillDisplay="\u200b";
       level="\u200b";
       effect="\u200b";
     }else{
       for(let i=0;i<tempArray.length;i++){
-        skillLevel+=tempArray[i][5];
-        skillLevel+="\n"
-        skillUpgrade+=tempArray[i][4];
-        skillUpgrade+="\n"
+        // skillLevel+=tempArray[i][5];
+        // skillLevel+="\n"
+        // skillUpgrade+=tempArray[i][4];
+        // skillUpgrade+="\n"
+        skillDisplay+=tempArray[i][5];
+        skillDisplay+=". ";
+        skillDisplay+=tempArray[i][4];
+        skillDisplay+="\n";
       }
     }
    
@@ -143,22 +156,80 @@ export default class skills implements IBotCommand {
     .setColor("RANDOM")
     .addField("Type",shikiSkill.type,true)
     .addField("Onibi",shikiSkill.onibi,true)
-    .addField(level+" and "+effect,skillLevel+" "+skillUpgrade,false)
+    .addField(level+" and "+effect,skillDisplay,false)
     // .addField(effect,skillUpgrade,true)
     for(let i=0;i<noteArray.length;i++){
       embed.addField(noteArray[i],noteArrayValue[i],true);
     }
     // msgObject.channel.send(embed)
     // .catch(console.error);
-    manageMessage(embed,msgObject);
+    return embed;
   }
 
+  async function sendMessage(embedArray:Discord.RichEmbed[],msgObject:Discord.Message){
 
-} 
+   const messageID= await msgObject.channel.send(embedArray[0]).then(msg =>{
+        return (msg as Discord.Message).id;
+    })
+  const msg= await msgObject.channel.fetchMessage(messageID);
+    
+setTimeout(() => {
+    sendMessage2(msg,msgObject,embedArray);
+}, 500);
+ 
 
-function manageMessage(embed:Discord.RichEmbed,msgObject:Discord.Message){
- const msgID= msgObject.channel.send(embed).then(msg =>{
-    return (msg as Discord.Message).id;
-  })
+  }
+
+  async function sendMessage2(msg:Discord.Message,msgObject:Discord.Message,embedArray:Discord.RichEmbed[]){
+ 
+    await msg.react('👍').then(() => msg.react('👎'));
+    const filter = (reaction: { emoji: { name: string; }; }, user: { id: string; }) => {
+        return ['👍', '👎'].includes(reaction.emoji.name) && user.id === msgObject.author.id;
+    };
+    messageReact(filter,msg,embedArray,0)
+
+  }
+
+  
+  function messageReact(filter:any,msg:Discord.Message,embedArray:Discord.RichEmbed[],index:number){
+
+
+    msg.awaitReactions(filter, { max:1, time: 300000, errors: ['time'] })
+    .then(collected => {
+        const botReaction = collected.first();
+ 
+        //    collected.first().remove("222728476816310272")
+            
+        
+            botReaction.users.forEach(user => {
+                if(user.id!="631907096555946028"){
+                    botReaction.remove(user.id);
+                }
+            });
+            
+       
+        if (botReaction.emoji.name === '👍') {
+            index--;
+            if(index<=-1){
+                index=3;
+            }
+            
+            
+       
+            msg.edit(embedArray[index]);
+            messageReact(filter,msg,embedArray,index);
+        } else {
+            index++;
+            if(index>=3){
+                index=0;
+            }
+     
+            msg.edit(embedArray[index]);
+            messageReact(filter,msg,embedArray,index);
+        }
+    })
+    .catch(collected => {
+        msg.delete();
+    });
 
 }
