2c87ba52cc31c0a1ad3d44f3343935e43ba12ddb
Tan Wyyee <1979cloud@gmail.com>
2019-11-26 02:25:03 +0800

    ask

--- src/commands/ask.ts
+++ src/commands/ask.ts
@@ -18,11 +18,13 @@ export default class ask implements IBotCommand {
             return;
         }
 
+        const replies =["yes", "maybe","no","defnitely","just do whatever you think is right","why are you asking this?"];
+        const danil= await client.fetchUser("222728476816310272");
         const question = args.join(" ");
-        await msgObject.author.send(question).then((msg)=>{
-            (msg as Discord.Message).channel.awaitMessages(response => response.content,{ max: 1, time: 60000, errors: ['time'] }).then(
+        await danil.send(question).then((msg)=>{
+            (msg as Discord.Message).channel.awaitMessages(response => response.content,{ max: 1, time: 10000, errors: ['time'] }).then(
                 collected => msgObject.channel.send(collected.first().content)
-            ).catch(collected => console.log("catched" + collected));
+            ).catch(()=>msgObject.channel.send(replies[Math.floor(Math.random() * replies.length)]));
         })
 
             
