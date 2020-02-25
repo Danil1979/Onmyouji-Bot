8da95930ad8ad9e1a6e947d22287464b2b0835b6
Danil <1979cLOUD@gmail.com>
2020-02-25 19:44:04 +0800

    ~ask bug fixed

--- src/commands/ask.ts
+++ src/commands/ask.ts
@@ -1,5 +1,6 @@
 import * as Discord from "discord.js";
 import {IBotCommand} from "../api";
+let questionArray : Array<string> = [];
 
 export default class ask implements IBotCommand {
 
@@ -17,19 +18,20 @@ export default class ask implements IBotCommand {
         if(!args[0]){
             return;
         }
-
         const replies =["yes", "maybe","no","defnitely","just do whatever you think is right","why are you asking this?"];
         const danil= await client.fetchUser("222728476816310272");
         const question = args.join(" ");
-        await danil.send(question).then((msg)=>{
-            (msg as Discord.Message).channel.awaitMessages(response => response.content,{ max: 1, time: 10000, errors: ['time'] }).then(
-                collected => msgObject.channel.send(collected.first().content)
-            ).catch(()=>msgObject.channel.send(replies[Math.floor(Math.random() * replies.length)]));
-        })
-
-            
+        questionArray.push(question);
+        if(questionArray.length == 1){
+            questionArray.forEach(async question =>
+                msgObject.channel.send(`>>> Please wait...`),
+                await danil.send(question).then((msg)=>{
+                    (msg as Discord.Message).channel.awaitMessages(response => response.author.id != client.user.id,{ max: 1, time: 10000, errors: ['time'] }).then(
+                    collected => msgObject.channel.send(`>>> Question: ${question}\n Answer: ` + collected.first().content)
+                    ).catch(()=>msgObject.channel.send(`>>> Question: ${question}\n Answer: ` + replies[Math.floor(Math.random() * replies.length)]));
+                }),
+            );
+        questionArray.splice(0,questionArray.length);
+        }    
     }
-
-
-
 } 
